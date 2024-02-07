2194dc60521dacd4343342c9d07bb5d3a8c06235
DavideSut <davidesut@gmail.com>
2024-02-07 14:53:43 +0100

    Added input text

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from autocorrect import Speller
 
+save_texts = False
 
 def API_call(page_ids):
-    # URL dell'API di Wikipedia
+    # Wikipedia API URL
     url = "https://en.wikipedia.org/w/api.php"
 
-    # Parametri della richiesta
+    # Request parameters
     params = {
         "action": "query",
         "format": "json",
@@ -26,21 +27,23 @@ def API_call(page_ids):
         "redirects": True,
     }
 
-    #titles = ["Medicine", "Python (programming language)"]
     texts = []
 
     for pd in page_ids:
         params["pageids"] = pd
-        # Effettua la richiesta all'API di Wikipedia
+        # Wikipedia API request
         response = requests.get(url, params=params)
-        # Estrai il testo della pagina di Wikipedia
+        # Page text extraction
         pages = response.json()["query"]["pages"]
         page_id = next(iter(pages))
         page_text = pages[page_id]["extract"]
         texts.append(page_text)
-        filename = "texts/" + str(page_id) + "_text.txt"
-        with open(filename, 'w', encoding="utf-8") as f:
-            f.write(page_text)
+        
+        # Save texts on files
+        if save_texts:
+            filename = "texts/" + str(page_id) + "_text.txt"
+            with open(filename, 'w', encoding="utf-8") as f:
+                f.write(page_text)
         
     return texts
 
@@ -72,36 +75,39 @@ def preprocessing(texts):
 
 
 def BOW(texts, pids):
-    # create the vocabulary
+    # Create the vocabulary
     vocab = set()
-    # create the bag-of-words model
+    # Create the bag-of-words model
     bow_model = []
 
     i = 0
     for text in texts:
-        # create a dictionary to store the word counts
+        # Create a dictionary to store the word counts
         word_counts = {}
-        # tokenize the text
+        # Tokenize the text
         tokens = nltk.word_tokenize(text)
-        # update the vocabulary
+        # Update the vocabulary
         vocab.update(tokens)
-        # count the occurrences of each word
+        # Count the occurrences of each word
         for word in tokens:
             if word in word_counts:
                 word_counts[word] += 1
             else:
                 word_counts[word] = 1
-        # add the word counts to the bag-of-words model
+        # Add the word counts to the bag-of-words model
         bow_model.append(word_counts)
 
-        filename = "bow/" + str(pids[i]) + "_bow.txt"
-        with open(filename, 'w', encoding="utf-8") as f:
-            f.write(str(word_counts))
-        i = i + 1
+        # Save bag-of-words models on files
+        if save_texts:
+            filename = "bow/" + str(pids[i]) + "_bow.txt"
+            with open(filename, 'w', encoding="utf-8") as f:
+                f.write(str(word_counts))
+            i = i + 1
     return bow_model 
 
 
 def add_label(texts, label):
+    # Add a the same label to multiple texts
     return [(text, label) for text in texts]
 
 def get_classifier(data):
@@ -111,36 +117,49 @@ def get_classifier(data):
     from sklearn.naive_bayes import MultinomialNB
     from sklearn.pipeline import Pipeline
 
+    # Create the pipeline
     pipeline = Pipeline([('tfidf', TfidfTransformer()),
                      ('chi2', SelectKBest(chi2, k=1000)),
                      ('nb', MultinomialNB())])
     
+    # Create the classifier
     classif = SklearnClassifier(pipeline)
-
+    # Train dataset
     classif.train(data)
 
     return classif
 
 
 if __name__ == "__main__":
-    cat_ids = ["18963910", "36248807", "71520442", "14389994", "38262946"] # Geography
-    not_cat_ids = ["23862", "18957", "6678", "19009110", "11145"] # Non-Geography
+    # Texts ids
+    cat_ids = ["18963910", "36248807", "71520442", "14389994", "38262946"] # Geographic
+    not_cat_ids = ["23862", "18957", "6678", "19009110", "11145"] # Non-Geographic
 
+    # Get texts from Wikipedia API
     cat_texts = API_call(cat_ids)
     not_cat_texts = API_call(not_cat_ids)
 
+    # Preprocessing
     cat_pp_texts = preprocessing(cat_texts)
     not_cat_pp_texts = preprocessing(not_cat_texts)
 
+    # Create BOW representation
     cat_bow_texts = BOW(cat_pp_texts, cat_ids)
     not_cat_bow_texts = BOW(not_cat_pp_texts, not_cat_ids)
 
-    data = add_label(cat_bow_texts, "Geographic") # Coppia (BOW, Category)
-    data += add_label(not_cat_bow_texts, "Non-Geographic") # Coppia (BOW, Non-Category)
+    # Create the training dataset
+    train_data = add_label(cat_bow_texts, "Geographic") # Coppia (BOW, Category)
+    train_data += add_label(not_cat_bow_texts, "Non-Geographic") # Coppia (BOW, Non-Category)
 
-    classifier = get_classifier(data)
+    # Get the classifier
+    classifier = get_classifier(train_data)
 
-    new_text = BOW(preprocessing(API_call(["19725260"])), ["19725260"])
+    #new_text = BOW(preprocessing(API_call(["19725260"])), ["19725260"])
 
-    cat = classifier.classify(new_text[0])
+    # Classify the new text
+    with open("input_text.txt", "r", encoding="utf-8") as f:
+        new_text = f.read()
+    new_bow_text = BOW(preprocessing([new_text]), ["new"])        
+
+    cat = classifier.classify(new_bow_text[0])
     print("The predicted category is: " + cat)
